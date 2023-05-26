34f0540696a7eba1ad6f681235b20feb3c98d91a
petrarca <pedropetrarcar@gmail.com>
2023-05-26 13:43:37 -0300

    feat: app base

---
+++ src/app.ts
@@ -0,0 +1,14 @@
+import "reflect-metadata";
+import "express-async-errors";
+import express from "express";
+import { appRoutes } from "./routes/index.routes";
+import { handleErrorMiddleware } from "./middlewares/handleError.middleware";
+
+const app = express();
+app.use(express.json());
+
+appRoutes(app);
+
+app.use(handleErrorMiddleware);
+
+export default app;
